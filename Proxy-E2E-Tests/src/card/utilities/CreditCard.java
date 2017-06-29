cd341519c57e255b5d2d73e9936a785cbde801da
Alberto Bandettini <alberto.bandettini@gmail.com>
2017-06-29 17:15:25 +0200

    CreditCard class updated

--- Proxy-E2E-Tests/src/card/utilities/CreditCard.java
+++ Proxy-E2E-Tests/src/card/utilities/CreditCard.java
@@ -1,5 +1,35 @@
 package card.utilities;
 
 public class CreditCard {
+	
+	private String pan;
+	private String name;
+	private String surname;
+	private String cvv;
+	private String expiry;
+	
+	public CreditCard (){
+		
+	}
+	
+	public String getPan (){
+		return this.pan;
+	}
+	
+	public String getExpiryDate(){
+		return this.expiry;
+	}
+
+	public String getCvv(){
+		return this.cvv;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public String getSurname(){
+		return this.surname;
+	}
 
 }
